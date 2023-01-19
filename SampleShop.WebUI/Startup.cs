22e6ef67ef2e53eccfed17b8056b4abf55f3d5fd
sztrelcsikzoltan <szzoltan@gmx.net>
2023-01-19 01:07:09 +0100

    Initial setup of projects

---
+++ SampleShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleShop.WebUI.Startup))]
+namespace SampleShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
